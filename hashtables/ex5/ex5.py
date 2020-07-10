997861b09656800b66cec0f27b431e96f868faa8
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-10 09:46:28 -0700

    Finish ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,20 @@
-# Your code here
+from collections import defaultdict
+from typing import List
 
 
-
-def finder(files, queries):
+def finder(files: List[str], queries: List[str]) -> List[str]:
     """
     YOUR CODE HERE
     """
-    # Your code here
 
-    return result
+    paths = defaultdict(list)
+
+    for full in files:
+        for path in full.split('/'):
+            if path:
+                paths[path].append(full)
+
+    return [full for path in queries if (full_paths := paths.get(path)) is not None for full in full_paths]
 
 
 if __name__ == "__main__":
