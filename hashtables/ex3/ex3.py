461285bc8be058d6bdb6632230f0bd500a4f8b53
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-10 08:24:49 -0700

    Complete intersection

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,33 @@
-def intersection(arrays):
+import itertools
+from typing import List
+
+
+def intersection(arrays: List[List[int]]) -> List[int]:
     """
     YOUR CODE HERE
     """
-    # Your code here
+
+    count = {}
+    result = []
+
+    for i in itertools.chain(*arrays):
+        try:
+            count[i] += 1
+        except KeyError:
+            count[i] = 1
+        else:
+            if count[i] == len(arrays):
+                result.append(i)
 
     return result
 
 
-if __name__ == "__main__":
-    arrays = []
-
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+def main() -> None:
+    arrays = [list(range(1000000, 2000000)) + [1, 2, 3], list(range(2000000, 3000000)) + [1, 2, 3],
+              list(range(3000000, 4000000)) + [1, 2, 3]]
 
     print(intersection(arrays))
+
+
+if __name__ == "__main__":
+    main()
